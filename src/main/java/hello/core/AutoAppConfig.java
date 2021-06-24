5ce81a050bc5fb23fda11d15bcdb976fda52d7ce
junho <junho6667@gmail.com>
2021-06-24 17:50:30 +0900

    2021/06/24

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,9 +13,11 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class))
 public class AutoAppConfig {
 
+/*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+*/
 
 }
---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+    }
+}
